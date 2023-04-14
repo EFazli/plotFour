32806601ab55a54c23f0c755650adb265db1aa3c
eli <elmira@gmail.com>
2023-04-15 02:25:24 +0430

    ...

--- src/main/java/com/ayria/plot4/Main.java
+++ src/main/java/com/ayria/plot4/Main.java
@@ -3,6 +3,7 @@ package com.ayria.plot4;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Plot Four");
         System.out.println("First player`s name:");
