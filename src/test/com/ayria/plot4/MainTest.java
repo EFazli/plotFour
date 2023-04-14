44b83b5dc70724e9e168358f39d5c3720570677f
eli <elmira@gmail.com>
2023-04-14 19:22:04 +0430

    write unit test for development

---
+++ src/test/com/ayria/plot4/MainTest.java
@@ -0,0 +1,38 @@
+package com.ayria.plot4;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void getDimention() {
+
+    }
+
+    @Test
+    void validateRow() {
+        assertEquals(true, Main.validateColumn(5));
+        assertEquals(true, Main.validateColumn(9));
+        assertEquals(false, Main.validateColumn(4));
+        assertEquals(false, Main.validateColumn(10));
+    }
+
+    @Test
+    void validateColumn() {
+        assertEquals(true, Main.validateColumn(5));
+        assertEquals(true, Main.validateColumn(9));
+        assertEquals(false, Main.validateColumn(4));
+        assertEquals(false, Main.validateColumn(10));
+    }
+
+}
