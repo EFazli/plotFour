dd693623edcd70e1731062b8e5414bbd6a96fa7b
eli <elmira@gmail.com>
2023-04-14 17:55:44 +0430

    initial the plot four game

---
+++ src/main/java/com/ayria/plot4/Plot4Application.java
@@ -0,0 +1,13 @@
+package com.ayria.plot4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plot4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plot4Application.class, args);
+	}
+
+}
